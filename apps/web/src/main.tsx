a21c9cbac24a503b38ed9e894502e72a74abf943
Md Azizul Islam <mdazizul8741@gmail.com>
2026-07-02 04:06:09 +0530

    refactor: simplify application bootstrap

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,2 +1,15 @@
 import "./styles/globals.css";
-import "./app/main";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./app/App";
+import { AppProviders } from "./app/providers";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </StrictMode>,
+);
